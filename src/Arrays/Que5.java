c16533ce24bc578815731e9d716951a075b3ed49
jay zade <jaykumarzade@gmail.com>
2024-03-09 17:55:30 +0530

    update

--- src/Arrays/Que5.java
+++ src/Arrays/Que5.java
@@ -1,10 +1,35 @@
 package Arrays;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Que5 {
     public static void main(String[] args) {
     int[] candies={2,3,5,1,3};
+        int extraCandies =3;
+
+        List<Boolean> ans = kidsWithCandies(candies, extraCandies);
+        System.out.println(ans);
 
     }
-    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+    static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        ArrayList<Boolean> list=new ArrayList<>();
+        int maxCandies=0;
+        for (int i = 0; i <candies.length ; i++) {
+            maxCandies=Math.max(candies[i],maxCandies);
+        }
 
+        for (int i = 0; i < candies.length; i++) {
+            candies [i] = candies[i]+extraCandies;
+            if(candies[i] >= maxCandies)
+            {
+               list.add(true);
+            }
+            else{
+                list.add(false);
+            }
+        }
+        return list;
+        }
     }
