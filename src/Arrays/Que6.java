f440e2294898c874f6a5f7924ae106ca8edc9e05
jay zade <jaykumarzade@gmail.com>
2024-03-10 18:40:01 +0530

    update 2 more classes

---
+++ src/Arrays/Que6.java
@@ -0,0 +1,30 @@
+package Arrays;
+
+import java.util.Arrays;
+
+//https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/description/
+public class Que6 {
+    public static void main(String[] args) {
+        int[] nums={8,1,2,2,3};
+        int[] ans = smallerNumbersThanCurrent(nums);
+        System.out.println(Arrays.toString(ans));
+    }
+    static int[] smallerNumbersThanCurrent(int[] nums) {
+                int[] arr=new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+
+                int count=0;
+            for(int j = 0; j < nums.length ; j++) {
+                {
+                    if(nums[i]>nums[j])
+                    {
+                        count++;
+                    }
+                }
+
+            }
+            arr[i]=count;
+        }
+        return arr;
+    }
+}
