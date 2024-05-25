05430bcf9660bae2b634272bc1fcf691eab4d4e6
jayzade <jaykumarzade@gmail.com>
2024-05-25 11:31:17 +0530

    update

--- src/HashSet/Pangram.java
+++ src/HashSet/Pangram.java
@@ -1,5 +1,7 @@
 package HashSet;
 
+import java.util.HashSet;
+
 public class Pangram {
 
     public static void main(String[] args) {
@@ -7,8 +9,20 @@ public class Pangram {
         checkIfPangram(sentence);
     }
 
-    public static boolean checkIfPangram(String sentence)
+    public static boolean checkIfPangram(String sentence) {
+        String sen = sentence.toLowerCase();
+        HashSet<Character> set = new HashSet<>();
+        for (char i = 'a'; i <= 'z'; i++) {
+            set.add(i);
+        }
 
+        for (int i = 0; i < sen.length(); i++) {
+            char current = sen.charAt(i);
+            if (current >= 'a' && current <= 'z') {
+                set.remove(current);
+            }
+        }
 
+        return set.isEmpty();
     }
 }
